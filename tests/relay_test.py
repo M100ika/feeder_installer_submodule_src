7c7c0a692d90d6d5381c051cb8321b90cebb98cd
M100ika <maxat.suiuebayev@gmail.com>
2024-10-26 10:48:58 +0500

    test

--- tests/relay_test.py
+++ tests/relay_test.py
@@ -1,11 +1,14 @@
 import sys
 from pathlib import Path
 
+import RPi.GPIO as GPIO
+
 sys.path.append(str(Path(__file__).parent.parent / 'src'))
 
 import _feeder_module as fdr
 from time import sleep
 from loguru import logger
+
 
 logger.remove()
 logger.add(sys.stderr, format="{time} {level} {message}", level="DEBUG")
@@ -20,6 +23,7 @@ def main():
             sleep(0.5)
     except KeyboardInterrupt or Exception as e:
         logger.info(f'Bye')
+        GPIO.cleanup()
 
 main()
 
