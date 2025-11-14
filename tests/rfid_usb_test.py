2e35880d9feef63d35e753a13549048f76e219b4
M100ika <maxat.suieubayev@gmail.com>
2025-11-14 16:50:46 +0500

    dd

---
+++ tests/rfid_usb_test.py
@@ -0,0 +1,23 @@
+import sys
+from pathlib import Path
+from loguru import logger
+import time
+sys.path.append(str(Path(__file__).resolve().parent.parent / 'src'))
+from _cha_2 import RFIDReader
+
+def on_tag(epc):
+    logger.info(f"🔷 Метка считана: {epc}")
+
+reader = RFIDReader()
+if not reader.reader_port:
+    port = reader.find_rfid_reader()
+    logger.info(f"Найден ридер: {port}")
+
+reader.open()
+reader.start_continuous_read(on_tag)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    reader.close()
